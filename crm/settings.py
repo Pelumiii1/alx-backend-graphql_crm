b30a5863dbd6406a4f5910d1b1798e3a2cd3e17b
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-08-28 09:23:42 +0100

    Heartbeat Logger with django-crontab

---
+++ crm/settings.py
@@ -0,0 +1,15 @@
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_crontab', 
+]
+
+
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+]
